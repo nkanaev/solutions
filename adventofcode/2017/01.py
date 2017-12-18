4ee329018a73048335b0bd4cff9e47717387a8fd
Nazar Kanaev <nkanaev@live.com>
2017-12-18 16:35:18 +0600

    bump

--- adventofcode/2017/01.py
+++ adventofcode/2017/01.py
@@ -5,13 +5,31 @@ def captchasum(data):
         if nums[(i + 1) % len(nums)] == n))
 
 
-def test():
+def captchasum2(data):
+    nums = [int(d) for d in data]
+    s = len(data) // 2
+    return sum((
+        n for i, n in enumerate(nums)
+        if nums[(i + s) % len(nums)] == n))
+
+
+def test1():
     assert captchasum('1122') == 3
     assert captchasum('1111') == 4
     assert captchasum('1234') == 0
     assert captchasum('91212129') == 9
 
 
+def test2():
+    assert captchasum2('1212') == 6
+    assert captchasum2('1221') == 0
+    assert captchasum2('123425') == 4
+    assert captchasum2('123123') == 12
+    assert captchasum2('12131415') == 4
+
+
 if __name__ == '__main__':
-    import sys
-    print(captchasum(sys.stdin.readline()))
+    with open('01.txt') as f:
+        data = f.readline()
+    print(captchasum(data))
+    print(captchasum2(data))
--- adventofcode/2017/02.py
+++ adventofcode/2017/02.py
@@ -2,7 +2,21 @@ def checksum(rows):
     return sum([max(r) - min(r) for r in rows])
 
 
-def test():
+def checksum2(rows):
+    result = 0
+    for row in rows:
+        for i in range(len(row) - 1):
+            for j in range(i + 1, len(row)):
+                if i == j:
+                    continue
+                a, b = max(row[i], row[j]), min(row[i], row[j])
+                if a % b == 0:
+                    result += a // b
+                    break
+    return result
+
+
+def test1():
     assert checksum([
         [5, 1, 9, 5],
         [7, 5, 3],
@@ -10,8 +24,17 @@ def test():
     ]) == 18
 
 
+def test2():
+    assert checksum2([
+        [5, 9, 2, 8],
+        [9, 4, 7, 3],
+        [3, 8, 6, 5],
+    ]) == 9
+
+
 if __name__ == '__main__':
-    import sys
-    lines = sys.stdin.readlines()
-    rows = [list(map(int, line.split())) for line in lines]
+    with open('02.txt') as f:
+        lines = f.readlines()
+        rows = [list(map(int, line.split())) for line in lines]
     print(checksum(rows))
+    print(checksum2(rows))
