92b51d95efac2f995f433991bc218371cc7ab614
Nazar Kanaev <nkanaev@live.com>
2018-12-01 23:44:09 +0000

    adventofcode 2018 #01

---
+++ adventofcode/2018/01.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int sol1(int *nums, size_t len) {
+    int result;
+    for (size_t i = 0; i < len; i++)
+        result += nums[i];
+    return result;
+}
+
+int sol2(int *nums, size_t len) {
+    int result;
+    int seen_pos[100000], seen_neg[100000];
+    for (size_t i = 0; i < 5000; i++) {
+        seen_pos[i] = 0;
+        seen_neg[i] = 0;
+    }
+    seen_pos[0] = 1;
+    while (1) {
+        for (size_t i = 0; i < len; i++) {
+            result += nums[i];
+            int *seen = (result >= 0) ? seen_pos : seen_neg;
+            seen[abs(result)]++;
+            if (seen[abs(result)] > 1)
+                return result;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int nums[10000], n;
+    size_t len = 0;
+    while (scanf("%d\n", &n) != EOF) {
+        nums[len] = n;
+        len++;
+    }
+    printf("%d\n", sol1(nums, len));
+    printf("%d\n", sol2(nums, len));
+    return 0;
+}
